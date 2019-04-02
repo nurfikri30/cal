920bd41ab00483cab4c6ed7d6cefd8c2991b840d
fikri <muhammadnurfikri286@gmail.com>
2019-04-02 12:06:21 +0700

    we

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihanmethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hasil;
+
+            Calculator calculator = new Calculator();
+
+            hasil = calculator.Penjumlahan(10, 2);
+            CetakHasil("hasil penjumlahan :" + hasil);
+
+
+            hasil = calculator.Penjumlahan(10, 2, 3);
+            CetakHasil("hasil penjumlahan overload :" + hasil);
+
+
+            hasil = calculator.Pengurangan(7, 2);
+            CetakHasil("hasil pengurangan :" + hasil);
+
+
+            hasil = Calculator.Perkalian(5, 2);
+            CetakHasil("hasil perkalian :" + hasil);
+
+            Console.ReadKey();
+
+            
+        }
+        static void CetakHasil(string hasil)
+        {
+            Console.WriteLine(hasil);
+        }
+    }
+}
---
+++ calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihanmethod
+{
+    public class Calculator
+    {
+        public int Penjumlahan(int a, int b)
+        {
+            return a + b;
+        }
+        public int Penjumlahan(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+        public int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
